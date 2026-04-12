b68b6d14b026332f9b7145ad6c1c3405be37437e
Ahmed-Ibrahem12 <ahmedibrahem8642@gmail.com>
2026-04-12 14:55:37 +0200

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-red-600 text-center text-4xl mt-5 font-bold text-shadow-lg">
+        elga3za el 3abd
+      </h1>
+    </div>
+  );
+};
+
+export default App;
